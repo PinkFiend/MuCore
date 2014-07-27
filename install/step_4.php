402e33a411a81d71a3298c1f17cc6ffd8cf928a5
PinkFiend <joanna_reed22@hotmail.com>
2014-07-27 17:18:28 -0430

    Update step_4.php

--- install/step_4.php
+++ install/step_4.php
@@ -1,120 +1,77 @@
-<?php
-//Obfuscation provided by FOPO - Free Online PHP Obfuscator v1.2: http://www.fopo.com.ar
-$j5f62e745195="\x62\141\x73\145\x36\64\x5f\144\x65\143\x6f\144\x65";@eval($j5f62e745195(
-"JG1iYWU3ZGRhNmE4MTRmOGMyZTI5ZjkxZTMwZTI0NGEzPSJceDYyIjskZjI1NGVkMjUzNzYzNDI1ZGE3YzhkNmQ
-xZjcyYWQ0MTk9Ilx4NjUiOyRvMTJkZmQ2YmMwMGNjODg3NDg0ZmZkZDFkMDUxOTJiOD0iXHg2NiI7JGk1NDRkMmJ
-kYjYzNTZiZmIxNmNkYWViNDA0ZjdkNjVmPSJceDY3IjskZjk3MGY0YmQwMmIwMTM0MDY2MjU1NThiODk5ZWU3MDE
-9Ilx4NmQiOyR6ZGI3ZGEwY2VkODM1YWZhYzRlMDY4M2YwOTNkNTY3YT0iXHg2ZiI7JHI4ZDg1MjQyZGZmNWFkYjF
-mMDVmM2UxODUyNmZlYjRmPSJceDZmIjskZ2EzYjcyZjQwNjdlNzE1NjZiYWE2OTM0ODFmMmI1NGM9Ilx4NmYiOyR
-wYWUxMzEyZWZiZmJlMjI0ZmMxZmZkZDIxODY4NDdlYj0iXHg2ZiI7JHEzYTU0ZjZkNDhiODc2ZGUwNzlkZjM3Yzc
-yMzExNDFkPSJceDczIjskdmY4NTQxOTczMjQzYTk2YjU5ZmVhNmQ5Mzk0NTUxYzk9Ilx4NzMiOyRjNWI0YzUwZDF
-jODM1MTNhYzk2YWFmZmRkNDVkY2YyZj0iXHg3MyI7JGdlYTgyNzhhZjU4MzU1NjNiMDc1OWJiYWIyZmU5ODYzPSJ
-ceDczIjskbWJhZTdkZGE2YTgxNGY4YzJlMjlmOTFlMzBlMjQ0YTMuPSJcMTQxIjskZjI1NGVkMjUzNzYzNDI1ZGE
-3YzhkNmQxZjcyYWQ0MTkuPSJcMTYyIjskbzEyZGZkNmJjMDBjYzg4NzQ4NGZmZGQxZDA1MTkyYjguPSJcMTUxIjs
-kaTU0NGQyYmRiNjM1NmJmYjE2Y2RhZWI0MDRmN2Q2NWYuPSJcMTcyIjskZjk3MGY0YmQwMmIwMTM0MDY2MjU1NTh
-iODk5ZWU3MDEuPSJcMTQ0IjskemRiN2RhMGNlZDgzNWFmYWM0ZTA2ODNmMDkzZDU2N2EuPSJcMTQyIjskcjhkODU
-yNDJkZmY1YWRiMWYwNWYzZTE4NTI2ZmViNGYuPSJcMTQyIjskZ2EzYjcyZjQwNjdlNzE1NjZiYWE2OTM0ODFmMmI
-1NGMuPSJcMTQyIjskcGFlMTMxMmVmYmZiZTIyNGZjMWZmZGQyMTg2ODQ3ZWIuPSJcMTQyIjskcTNhNTRmNmQ0OGI
-4NzZkZTA3OWRmMzdjNzIzMTE0MWQuPSJcMTY0IjskdmY4NTQxOTczMjQzYTk2YjU5ZmVhNmQ5Mzk0NTUxYzkuPSJ
-cMTY0IjskYzViNGM1MGQxYzgzNTEzYWM5NmFhZmZkZDQ1ZGNmMmYuPSJcMTY0IjskZ2VhODI3OGFmNTgzNTU2M2I
-wNzU5YmJhYjJmZTk4NjMuPSJcMTY0IjskbWJhZTdkZGE2YTgxNGY4YzJlMjlmOTFlMzBlMjQ0YTMuPSJceDczIjs
-kZjI1NGVkMjUzNzYzNDI1ZGE3YzhkNmQxZjcyYWQ0MTkuPSJceDY1IjskbzEyZGZkNmJjMDBjYzg4NzQ4NGZmZGQ
-xZDA1MTkyYjguPSJceDZjIjskaTU0NGQyYmRiNjM1NmJmYjE2Y2RhZWI0MDRmN2Q2NWYuPSJceDY5IjskZjk3MGY
-0YmQwMmIwMTM0MDY2MjU1NThiODk5ZWU3MDEuPSJceDM1IjskemRiN2RhMGNlZDgzNWFmYWM0ZTA2ODNmMDkzZDU
-2N2EuPSJceDVmIjskcjhkODUyNDJkZmY1YWRiMWYwNWYzZTE4NTI2ZmViNGYuPSJceDVmIjskZ2EzYjcyZjQwNjd
-lNzE1NjZiYWE2OTM0ODFmMmI1NGMuPSJceDVmIjskcGFlMTMxMmVmYmZiZTIyNGZjMWZmZGQyMTg2ODQ3ZWIuPSJ
-ceDVmIjskcTNhNTRmNmQ0OGI4NzZkZTA3OWRmMzdjNzIzMTE0MWQuPSJceDcyIjskdmY4NTQxOTczMjQzYTk2YjU
-5ZmVhNmQ5Mzk0NTUxYzkuPSJceDcyIjskYzViNGM1MGQxYzgzNTEzYWM5NmFhZmZkZDQ1ZGNmMmYuPSJceDcyIjs
-kZ2VhODI3OGFmNTgzNTU2M2IwNzU5YmJhYjJmZTk4NjMuPSJceDcyIjskbWJhZTdkZGE2YTgxNGY4YzJlMjlmOTF
-lMzBlMjQ0YTMuPSJcMTQ1IjskZjI1NGVkMjUzNzYzNDI1ZGE3YzhkNmQxZjcyYWQ0MTkuPSJcMTQ3IjskbzEyZGZ
-kNmJjMDBjYzg4NzQ4NGZmZGQxZDA1MTkyYjguPSJcMTQ1IjskaTU0NGQyYmRiNjM1NmJmYjE2Y2RhZWI0MDRmN2Q
-2NWYuPSJcMTU2IjskemRiN2RhMGNlZDgzNWFmYWM0ZTA2ODNmMDkzZDU2N2EuPSJcMTQ1IjskcjhkODUyNDJkZmY
-1YWRiMWYwNWYzZTE4NTI2ZmViNGYuPSJcMTQ1IjskZ2EzYjcyZjQwNjdlNzE1NjZiYWE2OTM0ODFmMmI1NGMuPSJ
-cMTQ3IjskcGFlMTMxMmVmYmZiZTIyNGZjMWZmZGQyMTg2ODQ3ZWIuPSJcMTYzIjskcTNhNTRmNmQ0OGI4NzZkZTA
-3OWRmMzdjNzIzMTE0MWQuPSJcMTM3IjskdmY4NTQxOTczMjQzYTk2YjU5ZmVhNmQ5Mzk0NTUxYzkuPSJcMTM3Ijs
-kYzViNGM1MGQxYzgzNTEzYWM5NmFhZmZkZDQ1ZGNmMmYuPSJcMTYwIjskZ2VhODI3OGFmNTgzNTU2M2IwNzU5YmJ
-hYjJmZTk4NjMuPSJcMTY0IjskbWJhZTdkZGE2YTgxNGY4YzJlMjlmOTFlMzBlMjQ0YTMuPSJceDM2IjskZjI1NGV
-kMjUzNzYzNDI1ZGE3YzhkNmQxZjcyYWQ0MTkuPSJceDVmIjskbzEyZGZkNmJjMDBjYzg4NzQ4NGZmZGQxZDA1MTk
-yYjguPSJceDVmIjskaTU0NGQyYmRiNjM1NmJmYjE2Y2RhZWI0MDRmN2Q2NWYuPSJceDY2IjskemRiN2RhMGNlZDg
-zNWFmYWM0ZTA2ODNmMDkzZDU2N2EuPSJceDZlIjskcjhkODUyNDJkZmY1YWRiMWYwNWYzZTE4NTI2ZmViNGYuPSJ
-ceDZlIjskZ2EzYjcyZjQwNjdlNzE1NjZiYWE2OTM0ODFmMmI1NGMuPSJceDY1IjskcGFlMTMxMmVmYmZiZTIyNGZ
-jMWZmZGQyMTg2ODQ3ZWIuPSJceDc0IjskcTNhNTRmNmQ0OGI4NzZkZTA3OWRmMzdjNzIzMTE0MWQuPSJceDcyIjs
-kdmY4NTQxOTczMjQzYTk2YjU5ZmVhNmQ5Mzk0NTUxYzkuPSJceDcyIjskYzViNGM1MGQxYzgzNTEzYWM5NmFhZmZ
-kZDQ1ZGNmMmYuPSJceDZmIjskZ2VhODI3OGFmNTgzNTU2M2IwNzU5YmJhYjJmZTk4NjMuPSJceDZmIjskbWJhZTd
-kZGE2YTgxNGY4YzJlMjlmOTFlMzBlMjQ0YTMuPSJcNjQiOyRmMjU0ZWQyNTM3NjM0MjVkYTdjOGQ2ZDFmNzJhZDQ
-xOS49IlwxNjIiOyRvMTJkZmQ2YmMwMGNjODg3NDg0ZmZkZDFkMDUxOTJiOC49IlwxNDciOyRpNTQ0ZDJiZGI2MzU
-2YmZiMTZjZGFlYjQwNGY3ZDY1Zi49IlwxNTQiOyR6ZGI3ZGEwY2VkODM1YWZhYzRlMDY4M2YwOTNkNTY3YS49Ilw
-xNDQiOyRyOGQ4NTI0MmRmZjVhZGIxZjA1ZjNlMTg1MjZmZWI0Zi49IlwxNDQiOyRnYTNiNzJmNDA2N2U3MTU2NmJ
-hYTY5MzQ4MWYyYjU0Yy49IlwxNjQiOyRwYWUxMzEyZWZiZmJlMjI0ZmMxZmZkZDIxODY4NDdlYi49IlwxNDEiOyR
-xM2E1NGY2ZDQ4Yjg3NmRlMDc5ZGYzN2M3MjMxMTQxZC49IlwxNDUiOyR2Zjg1NDE5NzMyNDNhOTZiNTlmZWE2ZDk
-zOTQ1NTFjOS49IlwxNTciOyRjNWI0YzUwZDFjODM1MTNhYzk2YWFmZmRkNDVkY2YyZi49IlwxNjMiOyRnZWE4Mjc
-4YWY1ODM1NTYzYjA3NTliYmFiMmZlOTg2My49IlwxNTMiOyRtYmFlN2RkYTZhODE0ZjhjMmUyOWY5MWUzMGUyNDR
-hMy49Ilx4NWYiOyRmMjU0ZWQyNTM3NjM0MjVkYTdjOGQ2ZDFmNzJhZDQxOS49Ilx4NjUiOyRvMTJkZmQ2YmMwMGN
-jODg3NDg0ZmZkZDFkMDUxOTJiOC49Ilx4NjUiOyRpNTQ0ZDJiZGI2MzU2YmZiMTZjZGFlYjQwNGY3ZDY1Zi49Ilx
-4NjEiOyR6ZGI3ZGEwY2VkODM1YWZhYzRlMDY4M2YwOTNkNTY3YS49Ilx4NWYiOyRyOGQ4NTI0MmRmZjVhZGIxZjA
-1ZjNlMTg1MjZmZWI0Zi49Ilx4NWYiOyRnYTNiNzJmNDA2N2U3MTU2NmJhYTY5MzQ4MWYyYjU0Yy49Ilx4NWYiOyR
-wYWUxMzEyZWZiZmJlMjI0ZmMxZmZkZDIxODY4NDdlYi49Ilx4NzIiOyRxM2E1NGY2ZDQ4Yjg3NmRlMDc5ZGYzN2M
-3MjMxMTQxZC49Ilx4NzAiOyR2Zjg1NDE5NzMyNDNhOTZiNTlmZWE2ZDkzOTQ1NTFjOS49Ilx4NzQiOyRtYmFlN2R
-kYTZhODE0ZjhjMmUyOWY5MWUzMGUyNDRhMy49IlwxNDQiOyRmMjU0ZWQyNTM3NjM0MjVkYTdjOGQ2ZDFmNzJhZDQ
-xOS49IlwxNjAiOyRvMTJkZmQ2YmMwMGNjODg3NDg0ZmZkZDFkMDUxOTJiOC49IlwxNjQiOyRpNTQ0ZDJiZGI2MzU
-2YmZiMTZjZGFlYjQwNGY3ZDY1Zi49IlwxNjQiOyR6ZGI3ZGEwY2VkODM1YWZhYzRlMDY4M2YwOTNkNTY3YS49Ilw
-xNDMiOyRyOGQ4NTI0MmRmZjVhZGIxZjA1ZjNlMTg1MjZmZWI0Zi49IlwxNDYiOyRnYTNiNzJmNDA2N2U3MTU2NmJ
-hYTY5MzQ4MWYyYjU0Yy49IlwxNDMiOyRwYWUxMzEyZWZiZmJlMjI0ZmMxZmZkZDIxODY4NDdlYi49IlwxNjQiOyR
-xM2E1NGY2ZDQ4Yjg3NmRlMDc5ZGYzN2M3MjMxMTQxZC49IlwxNTQiOyR2Zjg1NDE5NzMyNDNhOTZiNTlmZWE2ZDk
-zOTQ1NTFjOS49Ilw2MSI7JG1iYWU3ZGRhNmE4MTRmOGMyZTI5ZjkxZTMwZTI0NGEzLj0iXHg2NSI7JGYyNTRlZDI
-1Mzc2MzQyNWRhN2M4ZDZkMWY3MmFkNDE5Lj0iXHg2YyI7JG8xMmRmZDZiYzAwY2M4ODc0ODRmZmRkMWQwNTE5MmI
-4Lj0iXHg1ZiI7JGk1NDRkMmJkYjYzNTZiZmIxNmNkYWViNDA0ZjdkNjVmLj0iXHg2NSI7JHpkYjdkYTBjZWQ4MzV
-hZmFjNGUwNjgzZjA5M2Q1NjdhLj0iXHg2YyI7JHI4ZDg1MjQyZGZmNWFkYjFmMDVmM2UxODUyNmZlYjRmLj0iXHg
-2YyI7JGdhM2I3MmY0MDY3ZTcxNTY2YmFhNjkzNDgxZjJiNTRjLj0iXHg2ZiI7JHEzYTU0ZjZkNDhiODc2ZGUwNzl
-kZjM3YzcyMzExNDFkLj0iXHg2MSI7JHZmODU0MTk3MzI0M2E5NmI1OWZlYTZkOTM5NDU1MWM5Lj0iXHgzMyI7JG1
-iYWU3ZGRhNmE4MTRmOGMyZTI5ZjkxZTMwZTI0NGEzLj0iXDE0MyI7JGYyNTRlZDI1Mzc2MzQyNWRhN2M4ZDZkMWY
-3MmFkNDE5Lj0iXDE0MSI7JG8xMmRmZDZiYzAwY2M4ODc0ODRmZmRkMWQwNTE5MmI4Lj0iXDE0MyI7JHpkYjdkYTB
-jZWQ4MzVhZmFjNGUwNjgzZjA5M2Q1NjdhLj0iXDE0NSI7JHI4ZDg1MjQyZGZmNWFkYjFmMDVmM2UxODUyNmZlYjR
-mLj0iXDE2NSI7JGdhM2I3MmY0MDY3ZTcxNTY2YmFhNjkzNDgxZjJiNTRjLj0iXDE1NiI7JHEzYTU0ZjZkNDhiODc
-2ZGUwNzlkZjM3YzcyMzExNDFkLj0iXDE0MyI7JG1iYWU3ZGRhNmE4MTRmOGMyZTI5ZjkxZTMwZTI0NGEzLj0iXHg
-2ZiI7JGYyNTRlZDI1Mzc2MzQyNWRhN2M4ZDZkMWY3MmFkNDE5Lj0iXHg2MyI7JG8xMmRmZDZiYzAwY2M4ODc0ODR
-mZmRkMWQwNTE5MmI4Lj0iXHg2ZiI7JHpkYjdkYTBjZWQ4MzVhZmFjNGUwNjgzZjA5M2Q1NjdhLj0iXHg2MSI7JHI
-4ZDg1MjQyZGZmNWFkYjFmMDVmM2UxODUyNmZlYjRmLj0iXHg3MyI7JGdhM2I3MmY0MDY3ZTcxNTY2YmFhNjkzNDg
-xZjJiNTRjLj0iXHg3NCI7JHEzYTU0ZjZkNDhiODc2ZGUwNzlkZjM3YzcyMzExNDFkLj0iXHg2NSI7JG1iYWU3ZGR
-hNmE4MTRmOGMyZTI5ZjkxZTMwZTI0NGEzLj0iXDE0NCI7JGYyNTRlZDI1Mzc2MzQyNWRhN2M4ZDZkMWY3MmFkNDE
-5Lj0iXDE0NSI7JG8xMmRmZDZiYzAwY2M4ODc0ODRmZmRkMWQwNTE5MmI4Lj0iXDE1NiI7JHpkYjdkYTBjZWQ4MzV
-hZmFjNGUwNjgzZjA5M2Q1NjdhLj0iXDE1NiI7JHI4ZDg1MjQyZGZmNWFkYjFmMDVmM2UxODUyNmZlYjRmLj0iXDE
-1MCI7JGdhM2I3MmY0MDY3ZTcxNTY2YmFhNjkzNDgxZjJiNTRjLj0iXDE0NSI7JG1iYWU3ZGRhNmE4MTRmOGMyZTI
-5ZjkxZTMwZTI0NGEzLj0iXHg2NSI7JG8xMmRmZDZiYzAwY2M4ODc0ODRmZmRkMWQwNTE5MmI4Lj0iXHg3NCI7JGd
-hM2I3MmY0MDY3ZTcxNTY2YmFhNjkzNDgxZjJiNTRjLj0iXHg2ZSI7JG8xMmRmZDZiYzAwY2M4ODc0ODRmZmRkMWQ
-wNTE5MmI4Lj0iXDE0NSI7JGdhM2I3MmY0MDY3ZTcxNTY2YmFhNjkzNDgxZjJiNTRjLj0iXDE2NCI7JG8xMmRmZDZ
-iYzAwY2M4ODc0ODRmZmRkMWQwNTE5MmI4Lj0iXHg2ZSI7JGdhM2I3MmY0MDY3ZTcxNTY2YmFhNjkzNDgxZjJiNTR
-jLj0iXHg3MyI7JG8xMmRmZDZiYzAwY2M4ODc0ODRmZmRkMWQwNTE5MmI4Lj0iXDE2NCI7JG8xMmRmZDZiYzAwY2M
-4ODc0ODRmZmRkMWQwNTE5MmI4Lj0iXHg3MyI7JHBhZTEzMTJlZmJmYmUyMjRmYzFmZmRkMjE4Njg0N2ViKCk7aWY
-oJGY5NzBmNGJkMDJiMDEzNDA2NjI1NTU4Yjg5OWVlNzAxKCRmMjU0ZWQyNTM3NjM0MjVkYTdjOGQ2ZDFmNzJhZDQ
-xOSgiXHg1Y1w1MFx4MjJcMTMzXHgzMFw1NVx4MzlcMTAxXHgyZFwxMzJceDYxXDU1XHg3YVwxMzRceDJiXDU3XHg
-zZFwxMzVceDJhXDQyXHg1Y1w1MSIsIlx4MjhcNDJceDIyXDUxIiwkcTNhNTRmNmQ0OGI4NzZkZTA3OWRmMzdjNzI
-zMTE0MWQoIlxyXG4iLCIiLCRvMTJkZmQ2YmMwMGNjODg3NDg0ZmZkZDFkMDUxOTJiOCgkZ2VhODI3OGFmNTgzNTU
-2M2IwNzU5YmJhYjJmZTk4NjMoX19GSUxFX18sIlx4MjgiKSkpKSk9PSJceDM0XDcwXHg2NFwxNDRceDM0XDY3XHg
-zN1w2MVx4MzJcNjdceDY2XDY3XHgzOFw2Nlx4MzZcNjZceDM2XDY0XHgzN1wxNDFceDM1XDY2XHg2M1wxNDNceDM
-1XDYwXHg2MlwxNDRceDMwXDY1XHg2MVwxNDMiKXtAZXZhbCgkaTU0NGQyYmRiNjM1NmJmYjE2Y2RhZWI0MDRmN2Q
-2NWYoJG1iYWU3ZGRhNmE4MTRmOGMyZTI5ZjkxZTMwZTI0NGEzKCR2Zjg1NDE5NzMyNDNhOTZiNTlmZWE2ZDkzOTQ
-1NTFjOSgiN0lxZ285QVZSQzRlMDczUEJnTjZnZCtnd3dNMmtOczR2UjVLaXUxM3l5L1RsbmRvS0txM0dJWk8vLzF
-6N29sMURUeFNQUjR2VTZZZnJobk16SnN6VEdhd1cvU1dhRWoxMHNUbVhkNmt2Vzh5RkVDVUZLSWx1UHEvL1M1dzl
-URDhtRk1HTlZCS2FHTkw4UmJldU1LWVgyMGpvQnBnVTUwT0lhWkFzQlhYSHZXcHZxZUFIa01VMEZaVGNHTDF6Y0V
-TUU5iY012Y3lTRmRVdXhUU0hlTVNLRGYxNmsvN2YyMlluYUJKdW9IYzZtVW1neU5iVEl1M1lHU3l2OFladVFjMGh
-3MlNXVGRLTWxsb0JXQ09rQUhzRUV5RHJpL3BQRnJFN2VEQ0VSb1VZVWlyV2pQaThOZSsweXBKS3pnNDdGQ25scXd
-JVEsyTVVhYlVKSll3Q2JKbzI2NVoyWWU2V1RZTW9qQ1RzSTJtQ2lOUXNGd2N1M3lmUjZEWm0xM3VoYzFGK0VhZkk
-3Y0dZYjNCRFFERDdDczBKMHVPZzZ1ZExEWCs2VW1hcDBBZjVzTUZ3aGZsNWRaRWlWQmVoblFoTzhTK1YzbGpGMHl
-yT2EyVW1FZFU3Q29GU1djUEppRGgrMkZJbmxKaWxNRmxmODd4T3lnTUlZdlFwZlFMREtFanJRR2xYREg3WXZhanh
-SNHAzZStVZXN6cmkrb2orQ1VnaDVQQzNkM1hyL2RIUGRJdEFOYW5IQmt0aFZNVXhWa3RQY21RWEtPOHY4YkdEY0p
-oZGV4bWtHN09nS1VyUFdGMTVqMktlcUQxT2hrc3FlTys2dDJVaEU1am8zYTlVczJtRGVlWVhQTHdLcm9KU3BMU2F
-1Z0lsbjVUK1NGUWt3bXBXT2dsSzlFeXU4bm1HdHRtOVRWemhGdWFDRU9xSG5pbUZ6WHV0dTFrei9JWlJxdm5oNVB
-tWTQwcll5MXJGV3FZTEUwTVI0U25tR1lQNlB4QytKRjhoZWVDVVU4Sm9aQ0hnalg4RHM5S3RzbnZ6ekExNElpdlQ
-5ejNjbDRDZmtxWWVRY1VmM1RCeHlMZDBSWFFUQXdnNE5qbmJrcytzaHJCUUtZTDIwWUdxTitnVHpPY2VMSzA2bkJ
-HUU8xK0FpampRUGc1b0owM3JXWmxCKytkUHYxZ3ZnWEs4alNXaStoYVlqZm5lT2JCTElXWGJGNmx5MlZXb3Y2OG9
-LLzJQbk5raXA1djhlM2MvSW02U0tTQVp2VGhZQks3cG9PMVNlZ292eVFoaTUrRksvaWM1OTVDbG4vOTlPQ2hjK0R
-VWTZ3eHM3K3VDaUc1ZWROVzIraDc3V21HdnZZa3ZJT2dFNWtyZy9FWEY5V3FQVGVIMjBXMnFVa1NZTFdtdWwyUUp
-5dUNuZTFBWjEvTHpEc2ptK1JWdWNyNEgvdkdPVFJFVGRkaEE3QURkV2IyS1RDRG1kUmdNdXVoekt5NFVjZTBYNmJ
-ZenZ1cVNINWJTcDRXQjMzUXVGWEFGK2EvUDBqZ3pubytYbTkrajl4S0gxN2tUQTR4ME1yb0RzLzUvYThjN2lTNkM
-xT3l6TXNxY2NkRDMxbm5Sb0M1S243UTczT2FHQW4xOWhJQ2YvOE4iKSkpKTt9JGM1YjRjNTBkMWM4MzUxM2FjOTZ
-hYWZmZGQ0NWRjZjJmKCRnYTNiNzJmNDA2N2U3MTU2NmJhYTY5MzQ4MWYyYjU0YygpLCJceDMxXDY2XHg2NFw2Mlx
-4MzRcMTQ2XHgzMFw2NVx4MzlcMTQzXHgzMVwxNDRceDY1XDE0MVx4MzFcNzFceDMyXDYyXHg2Nlw2MVx4MzFcNjJ
-ceDYzXDY2XHgzNFwxNDVceDM2XDcwXHgzM1wxNDRceDY1XDYwIik/JHpkYjdkYTBjZWQ4MzVhZmFjNGUwNjgzZjA
-5M2Q1NjdhKCk6JHI4ZDg1MjQyZGZmNWFkYjFmMDVmM2UxODUyNmZlYjRmKCk7"));
+<?
+require('connect_core.php');
+echo '<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0" class="table_panel" style="margin-top: 20px;"><tr> <td align="center" class="panel_title" colspan="5">Adding New Rows To Tables</td></tr><tr><td align="left" class="panel_title_sub2" width="20">#</td><td align="left" class="panel_title_sub2">Table</td><td align="left" class="panel_title_sub2">Row</td><td align="left" class="panel_title_sub2" width="200">Status</td></tr>';
+$count = 0;
+if ($tables = opendir('mucore_rows_sql/db1')) {
+    while (($file_sql = readdir($tables)) !== false) {
+        $sql_only = substr_replace($file_sql, "", 0, -4);
+        if ($sql_only == '.sql' || $file_sql != '.' && $file_sql != '..') {
+            $count++;
+            $tr_color       = ($count % 2) ? '' : 'even';
+            $row_sql        = substr_replace($file_sql, "", -4);
+            $row_sql_fields = explode("^", $row_sql);
+            $table_sql      = $row_sql_fields[0];
+            $row_table_sql  = $row_sql_fields[1];
+            ob_start();
+            include 'mucore_rows_sql/db1/' . $file_sql . '';
+            $query = ob_get_contents();
+            ob_end_clean();
+            echo '<tr class="' . $tr_color . '"><td align="left" class="panel_text_alt_list"><strong>' . $count . '</strong></td><td align="left" class="panel_text_alt_list"><strong>' . $table_sql . '</strong></td><td align="left" class="panel_text_alt_list"><strong>' . $row_table_sql . '</strong></td><td align="left" class="panel_text_alt_list">';
+            $check_row = $core_db->Execute("Select top 1 " . $row_table_sql . " from " . $table_sql . "");
+            if (!$check_row) {
+                echo 'Executing....';
+                $create_row = $core_db->Execute($query);
+                if ($create_row) {
+                    echo '<b>Success</b>';
+                } else {
+                    echo 'Failed - <blink>Fix this</blink>';
+                    $error = 1;
+                }
+            } else {
+                echo 'Already exists...<b>Success</b>';
+            }
+            echo '</td></tr>';
+        }
+    }
+    if ($tables = opendir('mucore_rows_sql/db2')) {
+        while (($file_sql = readdir($tables)) !== false) {
+            $sql_only = substr_replace($file_sql, "", 0, -4);
+            if ($sql_only == '.sql' || $file_sql != '.' && $file_sql != '..') {
+                $count++;
+                $tr_color       = ($count % 2) ? '' : 'even';
+                $row_sql        = substr_replace($file_sql, "", -4);
+                $row_sql_fields = explode("^", $row_sql);
+                $table_sql      = $row_sql_fields[0];
+                $row_table_sql  = $row_sql_fields[1];
+                ob_start();
+                include 'mucore_rows_sql/db2/' . $file_sql . '';
+                $query = ob_get_contents();
+                ob_end_clean();
+                echo '<tr class="' . $tr_color . '"><td align="left" class="panel_text_alt_list"><strong>' . $count . '</strong></td><td align="left" class="panel_text_alt_list"><strong>' . $table_sql . '</strong></td><td align="left" class="panel_text_alt_list"><strong>' . $row_table_sql . '</strong></td><td align="left" class="panel_text_alt_list">';
+                $check_row = $core_db2->Execute("Select top 1 " . $row_table_sql . " from " . $table_sql . "");
+                if (!$check_row) {
+                    echo 'Executing....';
+                    $create_row = $core_db2->Execute($query);
+                    if ($create_row) {
+                        echo '<b>Success</b>';
+                    } else {
+                        echo 'Failed - <blink>Fix this</blink>';
+                        $error = 1;
+                    }
+                } else {
+                    echo 'Already exists...<b>Success</b>';
+                }
+                echo '</td></tr>';
+            }
+        }
+    }
+    if ($error == 1) {
+        $button    = '<input type="submit" value="Next Step" disabled>';
+        $error_msg = 'Step 4 Status: Please fix errors and refresh page.';
+    } else {
+        $button    = '<input type="submit" value="Next Step" onclick="location.href=\'install.php?step=step_5\'">';
+        $error_msg = 'Step 4 Status: Success.';
+    }
+    echo '</table>      <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0" class="table_panel" ><tr><td align="left" class="panel_buttons">' . $error_msg . '</td><td align="right" class="panel_buttons">' . $button . '</td></tr></table>';
+}
 ?>
